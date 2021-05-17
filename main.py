a16f1b9fc1e1652ab64dc38ad38129ce879159b4
David Hao <km101km@gmail.com>
2021-05-17 22:14:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
